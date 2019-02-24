38cec09fd1903ec4a19b3c3b766003749139d586
sunning <1576967298@qq.com>
2019-02-24 22:56:13 +0800

    菜单栏

--- private-pc/src/components/common-header.vue
+++ private-pc/src/components/common-header.vue
@@ -1,28 +1,11 @@
 <template>
   <div class="header-wrapper">
     <div class="header-title">
-      <i class="icon-home"></i>
-      <span class="header-home">智慧运营平台</span>
-      <el-menu
-        :default-active="activeIndex"
-        class="header add-header"
-        @select="change"
-        text-color="#06D3D3"
-        active-text-color="#06D3D3"
-        mode="horizontal">
-        <!-- <el-menu-item
-          class="item"
-          index="/service">
-          服务管理
-        </el-menu-item> -->
-        <el-menu-item
-          class="item"
-          index="/gaugeManage">
-          量表管理
-        </el-menu-item>
-      </el-menu>
+        <i class="icon-home"></i>
+      <span class="header-home">南阳市选民登记系统</span>
     </div>
     <div class="user">
+      <span>2018年12月23日 17:13:56 星期日</span>
       <span class="username">{{ userInfo.name, }}</span>
       <span
         class="quit"
@@ -43,9 +26,6 @@ export default {
     ...mapState('commonData', {
       userInfo: state => state.userInfo
     }),
-    activeIndex () {
-      return this.$route.path
-    }
   },
   created () {
     this.getUserInfo()
@@ -54,9 +34,6 @@ export default {
     ...mapActions('commonData', [
       'getUserInfo'
     ]),
-    change (index) {
-      this.$router.push({ path: index })
-    },
     quit () {
       this.$confirm('确认退出？')
         .then(()=> {
@@ -91,50 +68,38 @@ export default {
     width: 100%;
     height: 60px;
     padding:0px 30px 0 20px;
-    background:linear-gradient(180deg,rgba(250,254,255,1) 0%,rgba(239,248,248,1) 100%);
-    box-shadow:0px 2px 6px 0px rgba(0,0,0,0.22);
+    background-color: #d41c1a;
     display: flex;
     align-items: center;
     justify-content:space-between;
-    color: #06D3D3;
+    font-size: 12px;
+	color: #ffffff;
   }
   .icon-home {
-    background: url("../assets/img/home.png") center center no-repeat;
+    background: url("../assets/image/guohui.png") center center no-repeat;
     background-size: 100% 100%;
-    width: 36px;
-    height: 37px;
+    width: 39px;
+    height: 41px;
     display: inline-block;
     margin-right: 16px;
   }
   .header-home {
-    font-size: 18px;
-    font-weight: 500;
+    font-size: 28px;
+    color: #fdf4f4;
+    box-shadow: 0px 0px 6px 0px
+      rgba(34, 24, 21, 0.18);
   }
   .user{
     &>span{
       margin-left:20px;
     }
     & .username{
-      color: #333;
-      &:before {
-        content: "";
-        display: inline-block;
-        background: url("../assets/img/user.png") center center no-repeat;
-        background-size: 100% 100%;
-        width: 16px;
-        height: 16px;
-        margin-right: 6px;
-        transform: translateY(3px);
-      }
+      color: #ffffff;
     }
     & .quit{
       cursor: pointer;
       text-decoration: underline;
     }
-  }
-  .header {
-    margin-left: 80px;
-    background:linear-gradient(180deg,rgba(250,254,255,1) 0%,rgba(239,248,248,1) 100%);
   }
   .content {
     flex: 1;
--- private-pc/src/pages/common-layout.vue
+++ private-pc/src/pages/common-layout.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="view-wrapper">
     <common-header class="view-header" />
-    <router-view class="view-content" />
+    <div class="view-content">
+      <common-menu class="view-left" />
+      <div class="view-right">
+        <router-view class="view-right-content" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import CommonHeader from '../components/common-header'
+import CommonMenu from '../components/common-menu'
+import {mapState} from 'vuex'
 export default {
+  data () {
+
+    return {
+
+    }
+  },
   components: {
-    CommonHeader
+    CommonHeader,
+    CommonMenu
+  },
+  computed: {
+    ...mapState('commonData', {
+      breadList: state => state.breadList
+    })
+  },
+  watch: {
+    '$route' (to, from) {
+      let list = []
+      to.matched.forEach((item) => {
+        const {menuName} = item.meta
+        if (menuName) {
+          const names = menuName.split(',')
+          list = list.concat(names)
+        }
+      })
+      this.list = list
+      console.log(from)
+    }
   }
 }
 </script>
+<style>
+  .breadcrumb .el-breadcrumb__inner, .breadcrumb .el-breadcrumb__separator {
+    color: #000 !important;
+  }
+</style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .view-wrapper {
@@ -24,5 +62,33 @@ export default {
     flex: 1;
     display: flex;
     overflow: auto;
+  }
+  .view-left {
+    overflow: auto;
+  }
+  .view-right {
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    &-content {
+      flex: 1;
+      overflow: auto;
+    }
+  }
+  .view-breadcrumb {
+  }
+  .breadcrumb {
+    display: flex;
+    margin: 22px 42px 10px 42px;
+    border-bottom: 1px dashed #D3D3D3;
+    padding-bottom: 10px;
+    align-items: center;
+    min-height: 24px;
+  }
+  .breadcrumb-label {
+    font-size: 14px;
+    line-height: 1;
+    color: #000;
   }
 </style>
