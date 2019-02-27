2e3053cc80354bc5d6d99af19cea393da3921576
sunning <1576967298@qq.com>
2019-02-28 00:07:21 +0800

    申诉

--- public-pc/src/pages/complaint/index.vue
+++ public-pc/src/pages/complaint/index.vue
@@ -3,7 +3,7 @@
     <div class="title">在线申诉</div>
     <div class="form">
         <el-form
-        label-width="100px"
+        label-width="120px"
         :model="userLogin"
         :rules="rules"
         ref="loginForm"
@@ -13,20 +13,59 @@
           label="用户名："
           prop="username">
           <el-input
-            placeholder="请输入用户名"
-            :maxlength="18"
+            placeholder="请输入申诉人"
             class="item"
             v-model="userLogin.username" />
         </el-form-item>
         <el-form-item
           class="padding"
-          label="身份证号码"
+          label="身份证号码："
           prop="id_no">
           <el-input
             placeholder="请输入身份证号码"
             :maxlength="18"
             class="item"
             v-model="userLogin.username" />
+        </el-form-item>
+        <el-form-item
+          class="padding"
+          label="联系电话："
+          prop="phone">
+          <el-input
+            placeholder="请输入身份证号码"
+            :maxlength="11"
+            class="item"
+            v-model="userLogin.phone" />
+        </el-form-item>
+         <el-form-item
+          class="padding"
+          label="申诉书："
+          prop="fileList">
+            <el-upload
+              style="100%"
+              :class="['commom',{'uploadcomplait':userLogin.fileList.length>0}]"
+              :headers="headers"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              ref="upload"
+              :on-change="changeFile"
+              :on-success="successFn"
+              :on-error="errorFn"
+              :on-remove="changeFile"
+              :multiple="false"
+              :before-upload="beforeAvatarUpload"
+              :limit="1"
+              :file-list="userLogin.fileList"
+              :auto-upload="false">
+              <div
+              v-if="userLogin.fileList.length===0"
+              class="but">请上传申请书</div>
+            </el-upload>
+        </el-form-item>
+        <el-form-item
+        class="padding"
+         label=""
+        >
+             请根据<span class="down" @click="download">《公民申诉书》</span>模板，填写相应内容后，上传文件
         </el-form-item>
         <el-form-item
           label="验证码："
@@ -40,7 +79,7 @@
               v-model="userLogin.valid" />
             <div class="out-img"><img class="img" src="../../assets/img/home.png"/></div>
           </div>
-          <div class="change" @click="change">换一张</div>
+          <div class="change" @click="change">[换一张]</div>
         </el-form-item>
         <el-form-item class="padding butSize">
           <el-button
@@ -59,7 +98,8 @@
 
 <script>
 import { setSession } from '../../utils/session'
-import { login } from './service.js'
+import {taskDownload,login} from './service.js'
+import output from '../../utils/output.js'
 
 export default {
   data () {
@@ -67,7 +107,9 @@ export default {
       userLogin: {
         username: '',
         id_no: '',
-        valid: ''
+        phone: '',
+        valid: '',
+        fileList: []
       },
       rules: {
         username: [
@@ -76,13 +118,23 @@ export default {
         id_no: [
           { required: true, message: '请输入密码', trigger: 'blur' }
         ],
+        phone: [
+          { required: true, message: '请输入身份证号/手机号', trigger: 'blur' }
+        ],
         valid: [
           { required: true, message: '请输入验证码', trigger: 'blur' }
-        ]
+        ],
+        fileList:[{ required: true, message: '请输入申诉书', trigger: 'blur' }]
+      },
+      headers: {
+        // Authorization: authToken.token,
+        // AuthID: authToken.id
       }
     }
+
   },
   components: {
+
   },
   created () {
     document.addEventListener('keydown', this.enterSubmit, false)
@@ -110,6 +162,42 @@ export default {
     },
     cancelForm () {
 
+    },
+    submit () {
+      if (this.userLogin.fileList.length === 0) {
+        this.$notify.error({title: '上传文件不能为空'})
+        return
+      }
+      this.$emit('upload', {ref: 'upload'})
+      this.$refs.upload.submit()
+    },
+    changeFile (file, fileList) {
+      this.userLogin.fileList = fileList
+    },
+    beforeAvatarUpload (file) {
+      const isXlsx = file.type ===
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      if (!isXlsx) {
+        this.$notify.error({title: '只能上传 xlsx 格式的文件!'})
+      }
+      return isXlsx
+    },
+    successFn () {
+      this.$notify.success({title: '上传成功'})
+      this.$refs.upload.clearFiles()
+    },
+    errorFn (err) {
+      const { message = `{}` } = err
+      const errorObj = JSON.parse(message)
+      this.$notify.error({title: errorObj.message || '上传失败'})
+    },
+    async download (item) {
+      try {
+        const { data = {} } = await taskDownload({id: item.result_id,citizen_name: item.citizen_name,operate_name: item.name,doctor_name: item.doctor_name})
+        output({ data: data.data, download: data.filename, type: data.type })
+      } catch (err) {
+        console.log(err)
+      }
     },
   }
 }
@@ -203,4 +291,30 @@ export default {
  }
 
 
+.but {
+    width: 100%;
+    border: 1px solid #d8d8d8;
+    color: #999;
+    padding-left: 15px;
+
+  }
+  .uploadcomplait {
+    border: 1px solid #d8d8d8;
+  }
+  .down {
+    cursor: pointer;
+    color: blue;
+  }
+</style>
+<style>
+.commom .el-upload {
+  width: 100%;
+  text-align: left;
+}
+.commom .el-upload-list__item:first-child {
+  margin: 8px 0;
+}
+.uploadcomplait .el-upload{
+  display: none;
+}
 </style>
--- public-pc/src/pages/complaint/service.js
+++ public-pc/src/pages/complaint/service.js
@@ -17,3 +17,15 @@ export async function logout () {
   }
   // return api.delete('/api/v3/pcweb/users/session')
 }
+// 下载
+export async function taskDownload (payload) {
+  console.log(payload)
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {}
+      })
+    }, 500)
+  })
+  // return api.post(`/api/v3/pcweb/gauges/answers/${payload.id}/reports`,{citizen_name: payload.citizen_name,operate_name:payload.operate_name,doctor_name:payload.doctor_name})
+}
