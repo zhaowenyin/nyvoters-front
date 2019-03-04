eac4bd1a113afd677f7b0c69b82a3903dcef1cb5
sunning <1576967298@qq.com>
2019-03-04 16:46:08 +0800

    fix

--- small-program/src/router.js
+++ small-program/src/router.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/pages/home'
 
-const Task = () => import('@/pages/task')
-const Html = () => import('@/pages/task/html')
-const Result = () => import('@/pages/task/result')
-const Result2 = () => import('@/pages/task/result2')
-const Confirm = () => import('./pages/task/confirm')
-// 药品
-const Medicine = () => import('@/pages/task/medicine')
-const Selector = () => import('./pages/task/medicine/selector')
+
 
 Vue.use(Router)
 const router = new Router({
@@ -16,58 +10,8 @@ const router = new Router({
     {
       path: '/',
       name: '首页',
-      component: Task
+      component: Home
     },
-    {
-      path: '/task',
-      name: '任务模板',
-      component: Task
-    },
-    {
-      path: '/confirm',
-      name: '结果确认',
-      component: Confirm
-    },
-    {
-      path: '/result',
-      name: '结果',
-      component: Result
-    },
-    {
-      path: '/result2',
-      name: '结果2',
-      component: Result2
-    },
-    {
-      path: '/html',
-      name: '富文本',
-      component: Html
-    },
-    {
-      path: '/ai',
-      name: 'AI问诊',
-      component: AI
-    },
-    {
-      path: '/search',
-      name: 'AI搜索',
-      component: SearchAi
-    },
-    {
-      path: '/consider',
-      name: '考虑诊断',
-      component: Consider
-    },
-    {
-      path: '/medicine',
-      name: '药品',
-      component: Medicine
-    },
-    {
-      path: '/selector',
-      name: '选择',
-      component: Selector
-    }
 
   ]
 })
