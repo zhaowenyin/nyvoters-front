780dad144e9e41892afd55185d7ec6133acd5941
wenyin <342306219@qq.com>
2019-03-24 13:28:57 +0800

    搜索

--- private-pc/src/components/common-tree.vue
+++ private-pc/src/components/common-tree.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
+    <el-input
+      v-if="hasSearch"
+      placeholder="请输入关键字进行过滤"
+      size="medium"
+      class="input"
+      v-model="filterText">
+    </el-input>
     <el-tree
       class="add-tree"
+      ref="tree"
       icon-class="tree-icon"
+      :filter-node-method="filterNode"
       v-bind="[$attrs, $props]"
       v-on="$listeners"
       ></el-tree>
@@ -40,26 +49,38 @@ export default {
     highlightCurrent: {
       default: true,
       type: Boolean
+    },
+    hasSearch: {
+      default: false,
+      type: Boolean
     }
 
   },
   data () {
     return {
+      filterText: ''
     }
   },
   computed: {
 
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    }
   },
   components: {
   },
   created () {
   },
   methods: {
-    handleNodeClick(data) {
-      this.$emit('node-click', data)
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.name.indexOf(value) !== -1;
     }
   }
 }
 </script>
 <style scoped>
+
 </style>
