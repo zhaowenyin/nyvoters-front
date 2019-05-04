dbd8d6c363d1dd79efde3ade41e8a1cfdcf4aa9a
sunning <1576967298@qq.com>
2019-05-04 19:42:52 +0800

    fix

--- private-pc/src/pages/behalf-file-manage/service.js
+++ private-pc/src/pages/behalf-file-manage/service.js
@@ -1,4 +1,4 @@
-// import api from '../../../utils/api'
+import api from '../../utils/api'
 export async function getList (payload) {
   console.log(payload)
   return new Promise((resolve) => {
@@ -61,74 +61,74 @@ export async function getList (payload) {
   // return api.get('/disempower/list', { params: payload })
 }
 export async function getInfo (payload) {
-  console.log(payload)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        data: {
-          "code": "string",
-          "content": {
-            "belongAreaId": 54122356874,
-            "belongAreaName": "云阳镇第1选区",
-            "countyName": "南召县",
-            "day": 15,
-            "list": [
-              {
-                "age": "30",
-                "education": 1,
-                "gender": 1,
-                "idNum": 510810199910251100,
-                "nation": "HAN",
-                "party": 1,
-                "post": "文职",
-                "recommendPersonList": [
-                  "string"
-                ],
-                "recommendedPerson": "张三",
-                "status": "string",
-                "workUnit": "成都市文化旅游局"
-              },
-              {
-                "age": "20",
-                "education": 1,
-                "gender": 1,
-                "idNum": 510810199910251100,
-                "nation": "HAN",
-                "party": 1,
-                "post": "文职",
-                "recommendPersonList": [
-                  "string"
-                ],
-                "recommendedPerson": "张三",
-                "status": "string",
-                "workUnit": "成都市文化旅游局"
-              },
-              {
-                "age": "100",
-                "education": 1,
-                "gender": 1,
-                "idNum": 510810199910251100,
-                "nation": "HAN",
-                "party": 1,
-                "post": "文职",
-                "recommendPersonList": [
-                  "string"
-                ],
-                "recommendedPerson": "张三",
-                "status": "string",
-                "workUnit": "成都市文化旅游局"
-              }
-            ],
-            "month": 4,
-            "sessionNum": "六",
-            "year": 2019
-          },
-          "message": "string",
-          "status": "string"
-        }
-      })
-    }, 500)
-  })
-  // return api.post('/repre-affairs/word/list', payload)
+  // console.log(payload)
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve({
+  //       data: {
+  //         "code": "string",
+  //         "content": {
+  //           "belongAreaId": 54122356874,
+  //           "belongAreaName": "云阳镇第1选区",
+  //           "countyName": "南召县",
+  //           "day": 15,
+  //           "list": [
+  //             {
+  //               "age": "30",
+  //               "education": 1,
+  //               "gender": 1,
+  //               "idNum": 510810199910251100,
+  //               "nation": "HAN",
+  //               "party": 1,
+  //               "post": "文职",
+  //               "recommendPersonList": [
+  //                 "string"
+  //               ],
+  //               "recommendedPerson": "张三",
+  //               "status": "string",
+  //               "workUnit": "成都市文化旅游局"
+  //             },
+  //             {
+  //               "age": "20",
+  //               "education": 1,
+  //               "gender": 1,
+  //               "idNum": 510810199910251100,
+  //               "nation": "HAN",
+  //               "party": 1,
+  //               "post": "文职",
+  //               "recommendPersonList": [
+  //                 "string"
+  //               ],
+  //               "recommendedPerson": "张三",
+  //               "status": "string",
+  //               "workUnit": "成都市文化旅游局"
+  //             },
+  //             {
+  //               "age": "100",
+  //               "education": 1,
+  //               "gender": 1,
+  //               "idNum": 510810199910251100,
+  //               "nation": "HAN",
+  //               "party": 1,
+  //               "post": "文职",
+  //               "recommendPersonList": [
+  //                 "string"
+  //               ],
+  //               "recommendedPerson": "张三",
+  //               "status": "string",
+  //               "workUnit": "成都市文化旅游局"
+  //             }
+  //           ],
+  //           "month": 4,
+  //           "sessionNum": "六",
+  //           "year": 2019
+  //         },
+  //         "message": "string",
+  //         "status": "string"
+  //       }
+  //     })
+  //   }, 500)
+  // })
+  return api.post('/repre-affairs/word/list', payload)
 }
 
