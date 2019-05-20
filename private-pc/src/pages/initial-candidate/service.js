bbf265d61d8605e0a9e71664e30611d9135a456b
sunning <1576967298@qq.com>
2019-05-20 14:36:32 +0800

    fix

--- private-pc/src/pages/initial-candidate/service.js
+++ private-pc/src/pages/initial-candidate/service.js
@@ -2,58 +2,58 @@ import api from '../../utils/api'
 
 // get请求
 export async function getList (payload) {
-  console.log(payload)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        data:{
-          "code": "string",
-          "content": {
-            "data": [
-              {
-                "belongAreaId": 54122356874,
-                "belongAreaName": "云阳镇第1选区",
-                "birthDay": 605894400000,
-                "gender": 1,
-                "id": 1,
-                "idNum": 510810199910251100,
-                "phoneNum": 15898412568,
-                "recommendType": 1,
-                "recommendedPerson": "张三",
-                "recommendedPersonId": 12345678,
-                "status": "PRELIMINARY_CANDIDATE",
-                "type": 1
-              },
-              {
-                "belongAreaId": 54122356874,
-                "belongAreaName": "云阳镇第2选区",
-                "birthDay": 605894400000,
-                "gender": 1,
-                "id": 2,
-                "idNum": 510810199910251100,
-                "phoneNum": 15898412568,
-                "recommendType": 1,
-                "recommendedPerson": "张三",
-                "recommendedPersonId": 12345678,
-                "status": "PRELIMINARY_CANDIDATE",
-                "type": 1
-              }
-            ],
-            "endRow": 0,
-            "pageNum": 0,
-            "pageSize": 0,
-            "pages": 0,
-            "startRow": 0,
-            "total": 20
-          },
-          "message": "string",
-          "status": "string"
-        }
+  // console.log(payload)
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve({
+  //       data:{
+  //         "code": "string",
+  //         "content": {
+  //           "data": [
+  //             {
+  //               "belongAreaId": 54122356874,
+  //               "belongAreaName": "云阳镇第1选区",
+  //               "birthDay": 605894400000,
+  //               "gender": 1,
+  //               "id": 1,
+  //               "idNum": 510810199910251100,
+  //               "phoneNum": 15898412568,
+  //               "recommendType": 1,
+  //               "recommendedPerson": "张三",
+  //               "recommendedPersonId": 12345678,
+  //               "status": "PRELIMINARY_CANDIDATE",
+  //               "type": 1
+  //             },
+  //             {
+  //               "belongAreaId": 54122356874,
+  //               "belongAreaName": "云阳镇第2选区",
+  //               "birthDay": 605894400000,
+  //               "gender": 1,
+  //               "id": 2,
+  //               "idNum": 510810199910251100,
+  //               "phoneNum": 15898412568,
+  //               "recommendType": 1,
+  //               "recommendedPerson": "张三",
+  //               "recommendedPersonId": 12345678,
+  //               "status": "PRELIMINARY_CANDIDATE",
+  //               "type": 1
+  //             }
+  //           ],
+  //           "endRow": 0,
+  //           "pageNum": 0,
+  //           "pageSize": 0,
+  //           "pages": 0,
+  //           "startRow": 0,
+  //           "total": 20
+  //         },
+  //         "message": "string",
+  //         "status": "string"
+  //       }
 
-      })
-    }, 500)
-  })
-  // return api.post('/common/list',  payload )
+  //     })
+  //   }, 500)
+  // })
+  return api.post('/common/list',  payload)
 }
 
 export async function repeal (payload) {
