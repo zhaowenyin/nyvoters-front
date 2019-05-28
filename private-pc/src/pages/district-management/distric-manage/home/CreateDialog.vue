84796509e77d5b9d44fcb950d9d7524d315e5c7c
sunning <1576967298@qq.com>
2019-05-28 22:41:48 +0800

    fix

--- private-pc/src/pages/district-management/distric-manage/home/CreateDialog.vue
+++ private-pc/src/pages/district-management/distric-manage/home/CreateDialog.vue
@@ -118,10 +118,10 @@ export default {
       multipleSelection: [],
       rules: {
         name: [
-          { required: true, message: '选区编码', trigger: 'blur' }
+          { required: true, message: '选区名称', trigger: 'blur' }
         ],
         code: [
-          { required: true, message: '选区名称', trigger: 'change' }
+          { required: true, message: '选区编码', trigger: 'change' }
         ],
 
       },
