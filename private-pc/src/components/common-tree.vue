f918f31ce05b8a3b511a29a536cf8f0c6794e0c4
sunning <1576967298@qq.com>
2019-06-23 23:01:54 +0800

    fix

--- private-pc/src/components/common-tree.vue
+++ private-pc/src/components/common-tree.vue
@@ -31,7 +31,7 @@ export default {
         return {
           children: 'children',
           label: 'name',
-          disabled: 'leaf'
+          disabled: 'access'
         }
       },
       type: Object,
