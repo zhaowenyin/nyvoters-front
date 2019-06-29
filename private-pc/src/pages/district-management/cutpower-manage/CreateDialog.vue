781db6500fd540d6a516a6cc75a39122736505ef
sunning <1576967298@qq.com>
2019-06-29 16:23:36 +0800

    fix

--- private-pc/src/pages/district-management/cutpower-manage/CreateDialog.vue
+++ private-pc/src/pages/district-management/cutpower-manage/CreateDialog.vue
@@ -212,7 +212,7 @@ export default {
           { required: true, message: '请选择性别！', trigger: 'change' }
         ],
         nation: [
-          { required: true, message: '请选择民族！', trigger: 'blur' }
+          { required: true, message: '请选择民族！', trigger: 'change' }
         ],
         phoneNum: [
           { validator: validate,required: true, trigger: 'blur' }
