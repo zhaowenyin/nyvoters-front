3f210dfd9d84b52a5c7d8a6bb42d698ce723ccac
sunning <1576967298@qq.com>
2019-07-24 15:56:24 +0800

    fix

--- private-pc/src/common-data/module.js
+++ private-pc/src/common-data/module.js
@@ -37,8 +37,8 @@ export default {
         state.belongAreaId = state.belongAreaItem.id
         state.belongArea = state.belongAreaItem.name
         state.saveItem =  state.belongAreaItem
+        state.treeList = [payload.payload.data.content]
       }
-      state.treeList = [payload.payload.data.content]
     },
     updateNews(state, payload) {
       const list =  payload.data.content.data
@@ -68,8 +68,8 @@ export default {
           state.belongAreaId = state.saveItem.id
           state.belongArea = state.saveItem.name
           state.belongAreaItem = JSON.parse(JSON.stringify(state.saveItem))
+          return
         }
-        return
       }
       state.type = payload.type
       const { data } = await getTree({type: payload.type,id:payload.id})
