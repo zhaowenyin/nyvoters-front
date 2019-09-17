d1cdc0d982a44efaaeb62ae0497eb673e25cb8a8
royeer <599085046@qq.com>
2019-09-17 14:47:28 +0800

    增加资格审查记录列表-状态-查询

--- private-pc/src/common-data/config.js
+++ private-pc/src/common-data/config.js
@@ -96,10 +96,10 @@ export const statusList = {
 }
 // 资格选民状态
 export const QualficationStatusList = {
-  'DELETE': '删除',
-  'WAIT_SUBMIT': '待提交',
-  'WAIT_REVIEW': '待资格审查',
-  'REVIEW_FAIL': '资格审查不通',
+  //'DELETE': '删除',
+  //'WAIT_SUBMIT': '待提交',
+  //'WAIT_REVIEW': '待资格审查',
+  //'REVIEW_FAIL': '资格审查不通',
   'REVIEW_SUCCESS': '资格审查通过',
   'PRELIMINARY_CANDIDATE': '初步候选人',
   'FORMAL_CANDIDATE': '正式候选人',
