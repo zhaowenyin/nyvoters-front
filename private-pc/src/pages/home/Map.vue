847af1a66a03896a57910f56e9aae0779098894e
sunning <1576967298@qq.com>
2019-12-25 12:43:00 +0800

    fix

--- private-pc/src/pages/home/Map.vue
+++ private-pc/src/pages/home/Map.vue
@@ -8,7 +8,8 @@ export default {
     return {
       map: null,
       disProvince: null,
-      district: null
+      district: null,
+      regionMask: null
 
     }
   },
@@ -17,7 +18,6 @@ export default {
   },
   mounted() {
     this.initMap()
-    this.aa()
   },
   methods: {
     initMap(){
@@ -31,66 +31,149 @@ export default {
         mapStyle:'amap://styles/light',//
       });
       this.map = map;
-      this.district = new AMap.DistrictSearch({
-        showbiz:false,
-        subdistrict: 1,   //1 返回下一级行政区,0：不返回下级行政区
+      new AMap.TileLayer({
+        map: map,
+        // tileUrl: '../../assets/img/del.png', // s=Galil不加也能渲染
+        zIndex: 2, // 在默认层级之上
+        opacity: 0.1
+      })
+      this.locationSearch()
+      this.initPointSimplifier(map);
+    },
+    locationSearch () {
+      this.district =  new AMap.DistrictSearch({
+        subdistrict: 2,   ////返回下一级行政区。取值2，可以获取到上海的所有区
         extensions: 'all',  //返回行政区边界坐标组等具体信息
-        level: 'city'  //查询行政级别为市
-      });
-      // new AMap.TileLayer({
-      //   map: map,
-      //   // tileUrl: '../../assets/img/del.png', // s=Galil不加也能渲染
-      //   zIndex: 2, // 在默认层级之上
-      //   opacity: 0.1
-      // })
-      map.on('click',this.checkAndCloseInfo);
-      // this.search(410000,1)
-    },
-    checkAndCloseInfo (val) {
-      console.log(val)
-
-    },
-    search (region,dep) {
+        level: 'city'  //查询行政级别为 市
+      })
       let that = this
-      dep = typeof dep === 'undefined' ? 1 : dep;
-      this.disProvince && this.disProvince.setMap(null);
-      this.disProvince = new AMap.DistrictLayer.Province({
-        zIndex: 20,
-        adcode: [region],
-        depth: dep,
-        styles: {
-          'fill': function (properties) {
-            console.log(11,properties)
-            // properties为可用于做样式映射的字段，包含
-            // NAME_CHN:中文名称
-            // adcode_pro
-            // adcode_cit
-            // adcode
-            let adcode = properties.adcode;
-            return that.getColorByAdcode(adcode);
-          },
-          'province-stroke': '#fc9057',
-          'city-stroke': '#fc9057', // 中国地级市边界
-          'county-stroke': 'rgba(255,255,255,0.5)' // 中国区县边界
-        }
+      this.district.search("410000",function(status,result){
+        // 外多边形坐标数组和内多边形坐标数组
+        var outer = [
+          new AMap.LngLat(-360,90,true),
+          new AMap.LngLat(-360,-90,true),
+          new AMap.LngLat(360,-90,true),
+          new AMap.LngLat(360,90,true),
+        ];
+        var holes = result.districtList[0].boundaries
+        var pathArray = [
+          outer
+        ];
+        pathArray.push.apply(pathArray,holes)
+        var polygon = new AMap.Polygon({
+          pathL:pathArray,
+          strokeColor: '#00eeff',
+          strokeWeight: 1,
+          fillColor: 'rgba(255,255,255,1)',
+          fillOpacity: 0.5
+        });
+        polygon.setPath(pathArray);
+        that.map.add(polygon)
+        // var cityArr = result.districtList[0].districtList;
+        // for(var i=0;i<cityArr.length;i++){
+        //   that.showBoundary(cityArr[i].adcode);
+        // }
+      })
+    },
+    showBoundary (adcode) {
+      let that = this
+      this.district.search(adcode,function(status,result){
+        let color =  that.selectColor(result.districtList[0].adcode)
+        that.setMask(that.map,status,result.districtList[0].boundaries,color)
       })
 
-      this.disProvince.setMap(this.map);
+    },
+    setMask(map,status,bounds,color){
+      this.regionMask = new AMap.Polygon({
+        map:map,
+        path:bounds,
+        bubble:true,
+        zIndex:100,
+        strokeColor: 'rgb(20,164,173)',
+        strokeWeight: 10,
+        strokeOpacity:0,
+        fillColor: color,//'#000',
+        fillOpacity: 0.3,
+        strokeStyle:'solid',
+        strokeDasharray:[10,2,10]
+      });
+    },
+    initPointSimplifier () {
+      let that = this
+      AMapUI.loadUI(['geo/DistrictExplorer'], function(DistrictExplorer) {
+        //启动页面
+        that.initPage(DistrictExplorer);
+      });
+    },
+    initPage(DistrictExplorer) {
+      let that = this
+      //创建一个实例
+      var districtExplorer = new DistrictExplorer({
+        map: that.map //关联的地图实例
+      });
+
+      var adcode = 410000; //全国的区划编码
+
+      districtExplorer.loadAreaNode(adcode, function(error, areaNode) {
+
+        if (error) {
+          console.error(error);
+          return;
+        }
+        //绘制载入的区划节点
+        that.renderAreaNode(districtExplorer, areaNode);
+      });
+    },
+    renderAreaNode(districtExplorer, areaNode) {
+      let that = this
+
+      //清除已有的绘制内容
+      districtExplorer.clearFeaturePolygons();
+
+      //just some colors
+      var colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00"];
+
+      //绘制子级区划
+      districtExplorer.renderSubFeatures(areaNode, function(feature, i) {
+
+        var fillColor = colors[i % colors.length];
+        var strokeColor = colors[colors.length - 1 - i % colors.length];
+
+        return {
+          cursor: 'default',
+          bubble: true,
+          strokeColor: strokeColor, //线颜色
+          strokeOpacity: 1, //线透明度
+          strokeWeight: 1, //线宽
+          fillColor: fillColor, //填充色
+          fillOpacity: 0.35, //填充透明度
+        };
+      });
+
+      //绘制父级区划，仅用黑色描边
+      districtExplorer.renderParentFeature(areaNode, {
+        cursor: 'default',
+        bubble: true,
+        strokeColor: 'black', //线颜色
+        fillColor: null,
+        strokeWeight: 3, //线宽
+      });
+
+      //更新地图视野以适合区划面
+      that.map.setFitView(districtExplorer.getAllFeaturePolygons());
     },
     getColorByAdcode(adcode) {
       let colors = {}
       if (!colors[adcode]) {
-        // let gb = Math.random() * 155 + 50;
-        // colors[adcode] = 'rgb(' + gb + ',' + gb + ',255)';
         colors[adcode] = this.selectColor(adcode)
       }
       return colors[adcode];
     },
     selectColor (adcode) {
       // 信阳，三门峡市，平顶山市，焦作市，开封市，"鹤壁市"
-      let arr1 = [411500,411200,410400,410800,410200,410600]
+      let arr1 = ['411500','411200','410400','410800','410200','410600']
       //南阳市，济源市，漯河市，安阳市，商丘市
-      let arr2 = [411300,419001,411100,410500,411400]
+      let arr2 = ['411300','419001','411100','410500','411400']
       //洛阳市，"郑州市，驻马店，周口市，许昌市，新乡市，濮阳市
       // let arr3 = [410300,410100,411700,411600,411000,410700,410900]
       let color = '#fab98e'
@@ -100,61 +183,6 @@ export default {
         color = '#ffbe75'
       }
       return color
-
-    },
-    aa () {
-      let that = this
-      this.district.search('999',(status,result)=>{
-        that.setMaskOut(that.map,status,result,true);
-      })
-    },
-    setMaskOut(map,status,result,isAll){
-      // 外多边形坐标数组和内多边形坐标数组
-      var outer = [
-        new AMap.LngLat(-360,90,true),
-        new AMap.LngLat(-360,-90,true),
-        new AMap.LngLat(360,-90,true),
-        new AMap.LngLat(360,90,true),
-      ];
-      var holes = result.districtList[0].boundaries;
-
-      var pathArray = [outer];
-      pathArray.push.apply(pathArray,holes);
-      new AMap.Polygon({
-        map:map,
-        path:pathArray,
-        zIndex:5,
-        bubble:true,
-        strokeColor: 'rgb(20,164,173)',
-        strokeWeight: 2,
-        strokeOpacity:0,
-        fillColor: '#292452',//'#000',
-        fillOpacity: 0.7,
-        strokeStyle:'dashed',//轮廓线样式，实线:solid，虚线:dashed
-        strokeDasharray:[10,2,10]
-        /*勾勒形状轮廓的虚线和间隙的样式，此属性在strokeStyle 为dashed 时有效， 此属性在
-          ie9+浏览器有效 取值：
-          实线：[0,0,0]
-          虚线：[10,10] ，[10,10] 表示10个像素的实线和10个像素的空白（如此反复）组成的虚线
-          点画线：[10,2,10]， [10,2,10] 表示10个像素的实线和2个像素的空白 + 10个像素的实
-          线和10个像素的空白 （如此反复）组成的虚线*/
-      });
-      if(!isAll)return;
-      new AMap.Polygon({
-        map:map,
-        path:holes,
-        zIndex:5,
-        bubble:true,
-        strokeColor: 'rgb(20,164,173)',
-        strokeWeight: 2,
-        strokeOpacity:0,
-        fillColor: '#292452',//'#000',
-        fillOpacity: 0.7,
-        strokeStyle:'solid',
-        strokeDasharray:[10,2,10]
-      });
-
-      // this.regionGroup.add(poly);
     },
   }
 }
